3f447352aa5a97affcc38e87a7de12fe26a72407
José Albornoz <jojo@eljojo.net>
2026-02-10 13:07:26 -0500

    remove dead code

--- internal/wasm/main_create.go
+++ internal/wasm/main_create.go
@@ -18,12 +18,6 @@ func main() {
 	// Register bundle creation functions
 	js.Global().Set("rememoryCreateBundles", js.FuncOf(createBundlesJS))
 	js.Global().Set("rememoryParseProjectYAML", js.FuncOf(parseProjectYAMLJS))
-	js.Global().Set("rememoryGeneratePassphrase", js.FuncOf(generatePassphraseJS))
-	js.Global().Set("rememoryHashBytes", js.FuncOf(hashBytesJS))
-	js.Global().Set("rememoryEncryptAge", js.FuncOf(encryptAgeJS))
-	js.Global().Set("rememorySplitPassphrase", js.FuncOf(splitPassphraseJS))
-	js.Global().Set("rememoryCreateShare", js.FuncOf(createShareJS))
-	js.Global().Set("rememoryCreateTarGz", js.FuncOf(createTarGzJS))
 
 	// Signal that WASM is ready
 	js.Global().Set("rememoryReady", true)
